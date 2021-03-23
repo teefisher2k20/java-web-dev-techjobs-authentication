b0bc302f4cb31f0bebe300fe3438b9c866c2171b
teefisher2k20 <teefisher314@gmail.com>
2021-03-22 21:47:02 -0500

    studio 19

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
@@ -0,0 +1,70 @@
+package org.launchcode.javawebdevtechjobsauthentication;
+
+import org.launchcode.javawebdevtechjobsauthentication.controllers.AuthenticationController;
+import org.launchcode.javawebdevtechjobsauthentication.models.User;
+import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException {
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+        if (user != null) {
+            return true;
+        }
+        response.sendRedirect("/login");
+        return false;
+    }
+
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Configuration
+    public class WebApplicationConfig implements WebMvcConfigurer {
+
+        // Create spring-managed object to allow the app to access our filter
+        @Bean
+        public AuthenticationFilter authenticationFilter() {
+            return new AuthenticationFilter();
+        }
+
+        // Register the filter with the Spring container
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(authenticationFilter());
+        }
+
+    }
+
+}
