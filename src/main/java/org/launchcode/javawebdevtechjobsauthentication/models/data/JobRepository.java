3218c6def393b1b11cd39b9209776b69e625e58d
teefisher2k20 <teefisher314@gmail.com>
2021-03-24 10:39:38 -0500

    Added @Repository to JobRepository

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/JobRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/data/JobRepository.java
@@ -4,10 +4,7 @@ import org.launchcode.javawebdevtechjobsauthentication.models.Job;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-
 @Repository
-@Transactional
 public interface JobRepository extends CrudRepository<Job, Integer> {
 }
 
